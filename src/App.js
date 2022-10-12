306914fe8650cbd61b899963f69dbca9f67ed350
ShariqMehdi1234 <socialcast.dev0@gmail.com>
2022-10-13 01:40:51 +0500

    First Update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Headers from './Components/Headers';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Headers />
+    </React.Fragment>
+  );
+}
+
+export default App;
